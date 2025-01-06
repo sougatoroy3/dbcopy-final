f34e4c0fedb6a08f79472ba5698b8d3f2129d4ea
Sougato Roy <70825861+sougatoroy3@users.noreply.github.com>
2025-01-06 16:58:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,13 +43,13 @@ app.get('/fetch-status', async (req, res) => {
     
     // Configure based on the device
     if (device === 'VENUS') {
-      url = 'https://192.168.10.167/status.xml';
-      credentials = 'Test123:admin@123';
+      url = 'https://ipaddress/status.xml';
+      credentials = 'username:password';
     } 
     else if (device === 'JUPITER') 
     {
-      url = 'https://192.168.10.95/status.xml';
-      credentials = 'Test2:Vspl@1234';
+      url = 'https://ipaddress/status.xml';
+      credentials = 'username:password';
     } 
     else if (device === 'DC Cabin') 
     {
